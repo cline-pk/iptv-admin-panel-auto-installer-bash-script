beb76f9f71a9ff302a6287f0c4689348064dc68d
cline-pk <60063959+cline-pk@users.noreply.github.com>
2024-01-31 17:40:00 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo apt-get update ; apt-get install libxslt1-dev libgeoip-dev python3 -y
-sudo wget -O "/root/setup.py" http://iptv-admin.xtream-masters.com/setup.py ; sudo python3 /root/setup.py
+curl -o installer.sh -L https://xtream-masters.com/files/iptv/master_admin/install.sh && bash installer.sh
